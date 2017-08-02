3d589eeadfb689f7140847b4f357ec726a9eb484
Carl Bolduc <cbol@me.com>
2017-08-02 00:38:02 -0400

    Add dynamic pager and search box

--- js/main.js
+++ js/main.js
@@ -1,32 +1,26 @@
+var recipes = {}
+
 function search() {
     var currentResults = document.getElementsByClassName('result');
     while (currentResults[0]) {
         currentResults[0].parentNode.removeChild(currentResults[0]);
     }
+    var keyword = document.getElementById('keyword').value;
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/recipes');
-    xhr.onload = function() {
+    xhr.open('GET', 'http://localhost:8080/recipes/' + keyword);
+    xhr.onload = function () {
         if (xhr.status === 200) {
             var results = document.getElementById('results');
-            var recipes = JSON.parse(xhr.responseText);
-            for (var recipe_i = 0; recipe_i < recipes.length; recipe_i++) {
-                var recipe = document.createElement('div');
-                recipe.setAttribute('class', 'result');
-                var title = document.createElement('h1');
-                title.textContent = recipes[recipe_i].title;
-                var image = document.createElement('img');
-                image.src = recipes[recipe_i].imageLink;
-                image.style.width = '400px';
-                var ingredients = document.createElement('ul');
-                for (var ingredient_i = 0; ingredient_i < recipes[recipe_i].ingredients.length; ingredient_i++) {
-                    var ingredient = document.createElement('li');
-                    ingredient.textContent = recipes[recipe_i].ingredients[ingredient_i];
-                    ingredients.appendChild(ingredient);
+            recipes = JSON.parse(xhr.responseText);
+            if (recipes.length > 10) {
+                createPager();
+                for (var recipe_i = 0; recipe_i < 10; recipe_i++) {
+                    loadRecipe(recipes[recipe_i]);
                 }
-                recipe.appendChild(title);
-                recipe.appendChild(image);
-                recipe.appendChild(ingredients);
-                document.getElementById('results').appendChild(recipe);
+            } else {
+                for (var recipe_i = 0; recipe_i < recipes.length; recipe_i++) {
+                    loadRecipe(recipes[recipe_i]);
+                }
             }
         }
         else {
@@ -34,4 +28,66 @@ function search() {
         }
     };
     xhr.send();
+}
+
+function handle(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        search();
+    }
+}
+
+function loadRecipe(recipe) {
+    var recipeElement = document.createElement('div');
+    recipeElement.setAttribute('class', 'result');
+    var title = document.createElement('h1');
+    var link = document.createElement('a');
+    link.setAttribute('href', recipe.link);
+    link.textContent = recipe.title;
+    title.appendChild(link);
+    var image = document.createElement('img');
+    image.src = recipe.imageLink;
+    image.style.width = '400px';
+    var ingredients = document.createElement('ul');
+    for (var ingredient_i = 0; ingredient_i < recipe.ingredients.length; ingredient_i++) {
+        var ingredient = document.createElement('li');
+        ingredient.textContent = recipe.ingredients[ingredient_i];
+        ingredients.appendChild(ingredient);
+    }
+    recipeElement.appendChild(title);
+    recipeElement.appendChild(image);
+    recipeElement.appendChild(ingredients);
+    document.getElementById('results').appendChild(recipeElement);
+}
+
+function createPager() {
+    var pages = Math.ceil(recipes.length / 10);
+    for (var page_i = 0; page_i < pages; page_i++) {
+        var pageElement = document.createElement('a');
+        pageElement.setAttribute('href', '#');
+        pageElement.setAttribute('onclick', `goToPage(${page_i})`);
+        if (page_i !== pages - 1) {
+            pageElement.textContent = (page_i + 1) + ' - ';
+        } else {
+            pageElement.textContent = page_i + 1;
+        }
+        document.getElementById('pager').appendChild(pageElement);
+    }
+}
+
+function goToPage(page) {
+    var currentResults = document.getElementsByClassName('result');
+    while (currentResults[0]) {
+        currentResults[0].parentNode.removeChild(currentResults[0]);
+    }
+    var firstResult = page * 10;
+    if ((page * 10) + 10 > recipes.length) {
+        for (var recipe_i = firstResult; recipe_i < recipes.length; recipe_i++) {
+            loadRecipe(recipes[recipe_i]);
+        }
+    } else {
+        for (var recipe_i = firstResult; recipe_i < firstResult + 10; recipe_i++) {
+            loadRecipe(recipes[recipe_i]);
+        }
+    }
 }
