99683ab5c71879c6770df5843ab755d9da66443c
Carl Bolduc <cbol@me.com>
2018-01-25 20:17:57 -0500

    Add domReady script and put listeners in it

--- app.js
+++ app.js
@@ -12,63 +12,66 @@ requirejs.config({
 
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['app/pager', 'app/service', 'app/steward', 'app/utils'], function (pager, service, steward, utils) {
+requirejs(['domReady', 'app/pager', 'app/service', 'app/steward', 'app/utils'], function (domReady, pager, service, steward, utils) {
     var results = {};
 
-    window.addEventListener("popstate", function (e) {
-        if (e.state !== null) {
-            if (e.state.action === 'search') {
-                document.getElementById('keyword').value = e.state.keyword;
-                fetchResults(e.state.keyword, e.state.page)
-            } else if (e.state.action === 'home') {
-                actionHome();
+    domReady(function () {
+        window.addEventListener("popstate", function (e) {
+            if (e.state !== null) {
+                if (e.state.action === 'search') {
+                    document.getElementById('keyword').value = e.state.keyword;
+                    fetchResults(e.state.keyword, e.state.page)
+                } else if (e.state.action === 'home') {
+                    actionHome();
+                }
             }
-        }
-    });
-
-    if (location.search === "") {
-        history.pushState({'action': 'home'}, '', '');
-        service.getRecipesCount(function (serverData) {
-            var recipesCountElement = document.getElementById('recipes-count');
-            recipesCountElement.innerHTML = serverData;
         });
-    } else {
-        var keyword = decodeURIComponent(utils.getParameter('query'));
-        var page = utils.getParameter('page') === null ? 1 : parseInt(utils.getParameter('page'));
-        document.getElementById('keyword').value = keyword;
-        history.pushState({
-            'action': 'search',
-            'keyword': keyword,
-            'page': page
-        }, '', '?query=' + encodeURI(keyword) + '&page=' + page);
-        fetchResults(keyword, page)
-    }
 
-    document.getElementById('search-button').addEventListener('click', function (e) {
-        e.preventDefault();
-        actionSearch();
-    });
-
-    document.getElementById('keyword').addEventListener('keypress', function (e) {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            actionSearch();
-        }
-    });
-
-    document.getElementById('pager').addEventListener('click', function (e) {
-        if (e.target.className === 'page') {
-            e.preventDefault();
-            var keyword = decodeURI(utils.getParameter('query'));
-            var page = parseInt(e.target.textContent);
+        if (location.search === "") {
+            history.pushState({'action': 'home'}, '', '');
+            service.getRecipesCount(function (serverData) {
+                var recipesCountElement = document.getElementById('recipes-count');
+                recipesCountElement.innerHTML = serverData;
+            });
+        } else {
+            var keyword = decodeURIComponent(utils.getParameter('query'));
+            var page = utils.getParameter('page') === null ? 1 : parseInt(utils.getParameter('page'));
+            document.getElementById('keyword').value = keyword;
             history.pushState({
                 'action': 'search',
                 'keyword': keyword,
                 'page': page
             }, '', '?query=' + encodeURI(keyword) + '&page=' + page);
-            pager.goToPage(results, page);
+            fetchResults(keyword, page)
         }
+
+        document.getElementById('search-button').addEventListener('click', function (e) {
+            e.preventDefault();
+            actionSearch();
+        });
+
+        document.getElementById('keyword').addEventListener('keypress', function (e) {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                actionSearch();
+            }
+        });
+
+        document.getElementById('pager').addEventListener('click', function (e) {
+            if (e.target.className === 'page') {
+                e.preventDefault();
+                var keyword = decodeURI(utils.getParameter('query'));
+                var page = parseInt(e.target.textContent);
+                history.pushState({
+                    'action': 'search',
+                    'keyword': keyword,
+                    'page': page
+                }, '', '?query=' + encodeURI(keyword) + '&page=' + page);
+                pager.goToPage(results, page);
+            }
+        });
     });
+
 
     function actionHome() {
         document.getElementById('keyword').value = '';
@@ -77,7 +80,11 @@ requirejs(['app/pager', 'app/service', 'app/steward', 'app/utils'], function (pa
 
     function actionSearch() {
         var keyword = document.getElementById('keyword').value;
-        history.pushState({'action': 'search', 'keyword': keyword, 'page': 1}, '', '?query=' + encodeURI(keyword) + '&page=1');
+        history.pushState({
+            'action': 'search',
+            'keyword': keyword,
+            'page': 1
+        }, '', '?query=' + encodeURI(keyword) + '&page=1');
         fetchResults(keyword, 1);
     }
 
